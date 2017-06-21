9667d7d65d3dcaee52a7e2445586c4650bafd294
Julia <berrylily.0709@gmail.com>
2017-06-20 18:22:46 -0700

    Update 001_TwoSum solution

--- Algorithms/001_TwoSum/Solution.java
+++ Algorithms/001_TwoSum/Solution.java
@@ -1,25 +1,19 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int a = 0;
-        int b = 0;
-
-        int i1 = 0;
-        int i2 = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         for(int i = 0; i < nums.length; i++) {
-            a = nums[i];
-            i1 = i;
 
-            for(int j = i+1; j < nums.length; j++) {
-                b = nums[j];
-                i2 = j;
+            int n = nums[i];
 
-                if(a+b == target) {
-                    return new int[]{i1, i2};
-                }
+            if(map.containsKey(n)) {
+                return new int[] {map.get(n), i};
             }
+
+            map.put(target-n, i);
         }
-        return new int[]{0, 0};
+
+        return new int[] {0, 0};
     }
 }
