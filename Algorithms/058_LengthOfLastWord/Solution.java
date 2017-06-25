b17d01535930ba71aba0ea8ef3b2031e184ba94f
Julia <berrylily.0709@gmail.com>
2017-06-25 09:16:41 -0700

    Add 058_LengthOfLastWord solution

---
+++ Algorithms/058_LengthOfLastWord/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int lastWord = 0;
+        int l = s.length();
+
+        for(int j = l-1; j >= 0; j--) {
+            char c = s.charAt(j);
+
+            if(c == ' ') {
+                if(lastWord > 0) {
+                    break;
+                }
+                continue;
+            }
+            lastWord += 1;
+        }
+
+        return lastWord;
+    }
+}
