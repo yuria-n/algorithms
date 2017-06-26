2a2ea8034ddef5561a064be4019305ab197d221c
Julia <berrylily.0709@gmail.com>
2017-06-25 18:45:55 -0700

    Update 566_ReshapeTheMatrix solution

--- Algorithms/566_ ReshapeTheMatrix/Solution.java
+++ Algorithms/566_ ReshapeTheMatrix/Solution.java
@@ -1,25 +1,22 @@
 public class Solution {
     public int[][] matrixReshape(int[][] nums, int r, int c) {
 
+        int r1 = nums.length;
+        int c1 = nums[0].length;
+
+        if(r*c != r1*c1) {
+            return nums;
+        }
+
         int[][] nums2 = new int[r][c];
 
-        if(r*c == nums.length*nums[0].length) {
-
-            LinkedList<Integer> store = new LinkedList<Integer>();
-
-            for (int i = 0; i < nums.length; i++) {
-                for (int j = 0; j < nums[i].length; j++) {
-                    store.add(nums[i][j]);
-                }
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                int n = c1*i+j;
+                nums2[n/c][n%c] = nums[i][j];
             }
-
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) {
-                    nums2[i][j] = store.poll();
-                }
-            }
-            return nums2;
         }
-        return nums;
+
+        return nums2;
     }
 }
