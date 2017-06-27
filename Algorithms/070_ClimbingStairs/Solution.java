ad32b8e746cef58d475ec46e8cff354b2d440047
Julia <berrylily.0709@gmail.com>
2017-06-26 21:21:57 -0700

    Update 070_ClimbingStairs solution

--- Algorithms/070_ClimbingStairs/Solution.java
+++ Algorithms/070_ClimbingStairs/Solution.java
@@ -6,26 +6,12 @@ public class Solution {
         }
 
         int[] steps = {1, 2};
-
         int count = 0;
 
         for(int i = 0; i < steps.length; i++) {
             int step = steps[i];
 
-            if(step == n) {
-                count++;
-            }
-
-            if(step != n && step == 1) {
-                // 1つ前の結果をcountにたす(step == 1)
-                count += this.climbStairs(n-1);
-            }
-
-            if(step != n && step == 2) {
-                // stepが2のときn = 1のときの結果をたす
-                count += this.climbStairs(n-2);
-            }
-
+            count += step == n ? 1 : this.climbStairs(n-step);
         }
 
         return count;
