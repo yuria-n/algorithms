27b2e8209dadd85ec098d87a8b9419db5a947631
Julia <berrylily.0709@gmail.com>
2017-07-09 18:43:07 -0700

    Add 448_FindAllNumbersDisappearedInAnArray solution

---
+++ Algorithms/448_FindAllNumbersDisappearedInAnArray/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        int l = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for(int i = 0; i < l; i++) {
+            int n = nums[i];
+            map.put(n,n);
+        }
+
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for(int i = 1; i <= length; i++) {
+            if(!map.containsKey(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
