f852be0576ff84c3b394a9cbc8ef5109b672da31
Julia <berrylily.0709@gmail.com>
2017-08-06 15:09:38 -0700

    Update 067_AddBinary solution

--- Algorithms/067_AddBinary/Solution.java
+++ Algorithms/067_AddBinary/Solution.java
@@ -1,55 +1,28 @@
 public class Solution {
     public String addBinary(String a, String b) {
 
-        String result = "";
-
         int la = a.length();
         int lb = b.length();
         int l = Math.max(la, lb);
-
-        int next = 0;
 
         if(l == 0) {
             return "0";
         }
 
+        String result = "";
+        int next = 0;
+
         for (int j = l-1; j >= 0; j--) {
-            char a1;
-            if (j-(l-la) < 0) {
-                a1 = 0;
-            } else {
-                a1 = a.charAt(j-(l-la));
-            }
-            char b1;
-            if (j-(l-lb) < 0) {
-                b1 = 0;
-            } else {
-                b1 = b.charAt(j-(l-lb));
-            }
+            int ia = j-(l-la);
+            int a1 = ia < 0 ? 0 : a.charAt(ia) == '0' ? 0 : 1;
+            int ib = j-(l-lb);
+            int b1 = ib < 0 ? 0 : b.charAt(ib) == '0' ? 0 : 1;
 
-            int sum = 0 + next;
-            next = 0;
-            if(a1 == '1') {
-                sum++;
-            }
-            if(b1 == '1') {
-                sum++;
-            }
-            if(sum % 2 == 0) {
-                result = 0 + result;
-            }
-            if(sum % 2 == 1) {
-                result = 1 + result;
-            }
-            if(sum >= 2) {
-                next = 1;
-            }
+            int sum = a1 + b1 + next;
+            result = (sum % 2 == 0 ? 0 : 1 ) + result;
+            next = sum >= 2 ? 1 : 0;
         }
 
-        if(next != 0) {
-            result = 1 + result;
-        }
-
-        return result;
+        return next == 0 ? result : 1 + result;
     }
 }
