fd995d426d689fcb3d3ebd53c3478ae5fc474dae
Julia <berrylily.0709@gmail.com>
2017-08-06 17:13:18 -0700

    Use HashMap in 500_KeyboardRow solution

--- Algorithms/500_KeyboardRow/Solution.java
+++ Algorithms/500_KeyboardRow/Solution.java
@@ -1,52 +1,49 @@
 public class Solution {
+
+    private static final String[] LETTERS = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+
+    private static final HashMap<Character, Integer> MAP = new HashMap<>();
+
+    static {
+        for (int i = 0; i < LETTERS.length; i++) {
+            for (int j = 0; j < LETTERS[i].length(); j++) {
+                MAP.put(LETTERS[i].charAt(j), i);
+                MAP.put(Character.toUpperCase(LETTERS[i].charAt(j)), i);
+            }
+        }
+    }
+
     public String[] findWords(String[] words) {
 
-        String l1 = "qwertyuiop";
-        String l2 = "asdfghjkl";
-        String l3 = "zxcvbnm";
+        LinkedList<String> temporaryList = new LinkedList<>();
 
-        ArrayList<String> tmpArr = new ArrayList<String>();
+        for (int i = 0; i < words.length; i++) {
 
-        for(int i = 0; i < words.length; i++) {
             String w = words[i];
-            int t = -1;
-            char initial = Character.toLowerCase(w.charAt(0));
+            int value = MAP.get(w.charAt(0));
 
-            if(l1.indexOf(initial) >= 0) {
-                t = 1;
-            }
-            if(l2.indexOf(initial) >= 0) {
-                t = 2;
-            }
-            if(l3.indexOf(initial) >= 0) {
-                t = 3;
+            for (int j = 1; j < w.length(); j++) {
+
+                if (value == MAP.get(w.charAt(j))) {
+                    continue;
+                } else {
+                    value = -1;
+                    break;
+                }
             }
 
-            for(int j = 1; j < w.length(); j++) {
-                char c = Character.toLowerCase(w.charAt(j));
-                if(t == 1 && l1.indexOf(c) >= 0) {
-                    continue;
-                }
-                if(t == 2 && l2.indexOf(c) >= 0) {
-                    continue;
-                }
-                if(t == 3 && l3.indexOf(c) >= 0) {
-                    continue;
-                }
-                t = -1;
-                break;
-            }
-            if(t == 1 || t == 2 || t == 3) {
-                tmpArr.add(w);
+            if (value != -1) {
+                temporaryList.add(w);
             }
         }
 
-        int l = tmpArr.size();
+        int l = temporaryList.size();
         String[] results = new String[l];
 
-        for(int j = 0; j < l; j++) {
-            results[j] = tmpArr.get(j);
+        for (int i = 0; i < l; i++) {
+            results[i] = temporaryList.get(i);
         }
+
         return results;
     }
 }
