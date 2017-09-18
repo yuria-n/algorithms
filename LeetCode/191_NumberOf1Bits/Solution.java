c96f5015bd8641e55af29fd5b098179d6095aab5
Julia <berrylily.0709@gmail.com>
2017-09-17 23:21:31 -0700

    Update 191_NumberOfBits solution

--- LeetCode/191_NumberOf1Bits/Solution.java
+++ LeetCode/191_NumberOf1Bits/Solution.java
@@ -1,7 +1,15 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
+        int count = 0;
 
-    return Integer.bitCount(n);
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+
+        return count;
     }
 }
