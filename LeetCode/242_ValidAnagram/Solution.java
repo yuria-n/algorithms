398b3d8a94ccdae8942f8e92c4e3c61c3a275695
Julia <berrylily.0709@gmail.com>
2017-09-19 16:20:07 -0700

    Update 242_ValidAnagram solution

--- LeetCode/242_ValidAnagram/Solution.java
+++ LeetCode/242_ValidAnagram/Solution.java
@@ -6,24 +6,12 @@ class Solution {
             return false;
         }
 
-        HashMap<String, Integer> mapS = new HashMap<>();
-        HashMap<String, Integer> mapT = new HashMap<>();
-        for (int i = 0; i < l; i++) {
-            String c1 = s.substring(i, i + 1);
-            int count1 = mapS.containsKey(c1) ? mapS.get(c1) + 1 : 1;
-            mapS.put(c1, count1);
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
 
-            String c2 = t.substring(i, i + 1);
-            int count2 = mapT.containsKey(c2) ? mapT.get(c2) + 1 : 1;
-            mapT.put(c2, count2);
-        }
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
 
-        for (Object o : mapS.entrySet()) {
-            Map.Entry m = (Map.Entry) o;
-            if (!mapT.entrySet().contains(m)) {
-                return false;
-            }
-        }
-        return true;
+        return Arrays.equals(arrS, arrT);
     }
 }
