7f6ad5877dda9dc57b4842d0d0cd614edd55f3b1
Julia <berrylily.0709@gmail.com>
2017-10-17 14:06:02 -0700

    Update Grading Students solution

--- HackerRank/011_GradingStudents/Solution.java
+++ HackerRank/011_GradingStudents/Solution.java
@@ -4,32 +4,33 @@ import java.util.*;
 
 public class Solution {
 
-    static int[] solve(int[] grades) {
-        int l = grades.length;
-        int[] finals = new int[l];
+  static int[] solve(int[] grades) {
+      int l = grades.length;
+      int[] finals = new int[l];
 
-        for (int i = 0; i < l; i++) {
-            int g = grades[i];
-            if (g < 38 || (g >= 38 && g % 5 < 3)) {
-                finals[i] = g;
-            } else {
-                finals[i] = g + (5 - g % 5);
-            }
-        }
-        return finals;
-    }
+      for (int i = 0; i < l; i++) {
+          int g = grades[i];
+          int r = g % 5;
+          if (g < 38 || r < 3) {
+              finals[i] = g;
+          } else {
+              finals[i] = g + (5 - r);
+          }
+      }
+      return finals;
+  }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] grades = new int[n];
-        for (int grades_i = 0; grades_i < n; grades_i++) {
-            grades[grades_i] = in.nextInt();
-        }
-        int[] result = solve(grades);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
-        }
-        System.out.println("");
-    }
+  public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+      int[] grades = new int[n];
+      for (int grades_i = 0; grades_i < n; grades_i++) {
+          grades[grades_i] = in.nextInt();
+      }
+      int[] result = solve(grades);
+      for (int i = 0; i < result.length; i++) {
+          System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+      }
+      System.out.println("");
+  }
 }
