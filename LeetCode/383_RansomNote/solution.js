8b6b563d45434b293efed0f1be1fa66a7f895ebf
Julia <berrylily.0709@gmail.com>
2017-11-18 11:51:11 -0800

    Update 383_RansomNote solution (js)

--- LeetCode/383_RansomNote/solution.js
+++ LeetCode/383_RansomNote/solution.js
@@ -7,20 +7,14 @@ const canConstruct = function(ransomNote, magazine) {
     const map = {};
     for (let i = 0; i < magazine.length; i++) {
         const key = magazine[i];
-        const count = map[key] ? map[key] + 1 : 1;
-        map[key] = count;
+        map[key] = map[key] ? map[key] + 1 : 1;
+        // map[key] = ++map[key] || 1;
     }
     
     for (let i = 0; i < ransomNote.length; i++) {
         const key = ransomNote[i];
-        if (!map[key]) {
+        if (!map[key]--) {
             return false;
-        }
-        const count = map[key] - 1;
-        if (!count) {
-            delete map[key];
-        } else {
-            map[key] = count;
         }
     }
     return true;
