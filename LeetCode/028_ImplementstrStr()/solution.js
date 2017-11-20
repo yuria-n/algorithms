8ae729539ff2209726000b6838ea3f7746f64596
Julia <berrylily.0709@gmail.com>
2017-11-20 11:34:10 -0800

    Refactor solution

--- LeetCode/028_ImplementstrStr()/solution.js
+++ LeetCode/028_ImplementstrStr()/solution.js
@@ -6,34 +6,26 @@
 const strStr = function(haystack, needle) {
     const lh = haystack.length;
     const ln = needle.length;
-    if (lh < needle) {
-        return -1;
-    }
     if (!ln) {
         return 0;
     }
     
     let n0 = needle[0];
-    for (let i = 0; i < lh; i++) {
+    
+    loop:
+    for (let i = 0; i < lh - ln + 1; i++) {
         let h = haystack[i];
         if (h !== n0){
             continue;
         }
 
-        for (let j = 0; j < ln; j++) {
-            const n1 = needle[j];
-            const h1 = haystack[i + j];
-            if (n1 !== h1) {
-                break;
-            }
-            if (j === ln - 1) {
-                return i;
+        for (let j = 1; j < ln; j++) {
+            if (needle[j] !== haystack[i + j]) {
+                continue loop;
             }
         }
 
-        if (lh - i <= ln) {
-            break;
-        }
+        return i;
     }
     
     return -1;
