6b4c97c040a87b5aa478f0cd1c0577bf6d7c3bd3
Julia <berrylily.0709@gmail.com>
2017-11-21 11:11:51 -0800

    Update solution

--- LeetCode/389_FindTheDifference/solution.js
+++ LeetCode/389_FindTheDifference/solution.js
@@ -4,18 +4,17 @@
  * @return {character}
  */
 const findTheDifference = function(s, t) {
-    const map = {};
+    const arr = Array(26).fill(0);
     for (let i = 0; i < s.length; i++) {
         const c = s[i];
-        const v = map[c] + 1 || 1;
-        map[c] = v;
+        arr[c.charCodeAt() - 97]++;
     }
-    
+
     for (let i = 0; i < t.length; i++) {
         const c = t[i];
-        if (!map[c]) {
+        if (!arr[c.charCodeAt() - 97]) {
             return c;
         }
-        map[c]--;
+        arr[c.charCodeAt() - 97]--;
     }
 };
