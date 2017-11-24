13b834c5ad3874e2f3cadfb30cad63976b4b290a
Julia <berrylily.0709@gmail.com>
2017-11-24 14:04:18 -0800

    Update solution

--- LeetCode/171_Excel SheetColumnNumber/solution.js
+++ LeetCode/171_Excel SheetColumnNumber/solution.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 const titleToNumber = function(s) {
+    const l = s.length - 1;
     let result = 0;
-    let c = 0;
-    for (let i = s.length - 1; i >= 0; i--) {
+    for (let i = l; i >= 0; i--) {
+        const c = l - i;
         const n = s[i].charCodeAt() - 64;
         result += n * Math.pow(26, c);
-        c++;
     }
     return result;
 };
