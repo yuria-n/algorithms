67faa7a1ef549f41475028e384450fd9c62f757c
Julia <berrylily.0709@gmail.com>
2018-01-05 18:40:30 -0800

    update solution (js)

--- LeetCode/067_AddBinary/solution.js
+++ LeetCode/067_AddBinary/solution.js
@@ -8,36 +8,18 @@ const addBinary = function(a, b) {
   const lb = b.length;
   const l = Math.max(la, lb);
 
-  if (!l) {
-    return "0";
-  }
+  if (!l) return '0';
 
-  let result = "";
+  let result = '';
   let m = 0;
   for (let i = l - 1; i >= 0; i--) {
     const ia = i - (l - la);
     const ib = i - (l - lb);
     const da = ia < 0 ? 0 : +a[ia];
     const db = ib < 0 ? 0 : +b[ib];
-    console.log(`da: ${da}  db: ${db}  m: ${m}`);
     const sum = da + db + m;
-    switch (sum) {
-      case 0:
-        result = "0" + result;
-        m = 0;
-        break;
-      case 1:
-        result = "1" + result;
-        m = 0;
-        break;
-      case 2:
-        result = "0" + result;
-        m = 1;
-        break;
-      default:
-        result = "1" + result;
-        m = 1;
-    }
+    result = (sum % 2 === 0 ? 0 : 1) + result;
+    m = sum < 2 ? 0 : 1;
   }
 
   return !m ? result : 1 + result;
