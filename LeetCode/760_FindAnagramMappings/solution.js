505bae868f10780b98b3bd6633546ef0d9c25ba8
Julia <berrylily.0709@gmail.com>
2018-01-06 22:45:36 -0800

    update solution (js)

--- LeetCode/760_FindAnagramMappings/solution.js
+++ LeetCode/760_FindAnagramMappings/solution.js
@@ -4,13 +4,15 @@
  * @return {number[]}
  */
 const anagramMappings = function(A, B) {
+  const l = A.length;
+
   const map = {};
-  for (let i = 0; i < B.length; i++) {
+  for (let i = 0; i < l; i++) {
     map[B[i]] = i;
   }
 
-  const result = [];
-  for (let i = 0; i < A.length; i++) {
+  const result = new Array(l);
+  for (let i = 0; i < l; i++) {
     result[i] = map[A[i]];
   }
 
