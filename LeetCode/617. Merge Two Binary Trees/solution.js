0f57d479240044a96066eea511af39d7f2186862
Julia <berrylily.0709@gmail.com>
2018-03-29 10:29:38 -0700

    Update 617. Merge Two Binary Trees solution (js)

--- LeetCode/617. Merge Two Binary Trees/solution.js
+++ LeetCode/617. Merge Two Binary Trees/solution.js
@@ -11,21 +11,17 @@
  * @return {TreeNode}
  */
 const mergeTrees = function(t1, t2) {
-  if (!t1 && !t2) {
-    return null;
+  if (!t1) {
+    return t2;
+  }
+  if (!t2) {
+    return t1;
   }
 
-  let t = new TreeNode();
-  if (t1 && t2) {
-    t.val = t1.val + t2.val;
-  } else if (t1) {
-    t.val = t1.val;
-  } else if (t2) {
-    t.val = t2.val;
-  }
+  t1.val += t2.val;
 
-  t.left = mergeTrees(t1 ? t1.left : null, t2 ? t2.left : null);
-  t.right = mergeTrees(t1 ? t1.right : null, t2 ? t2.right : null);
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
 
-  return t;
+  return t1;
 };
