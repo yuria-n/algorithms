856a52fdc4b31d9be443fd85507bc022fbc4924d
Julia <berrylily.0709@gmail.com>
2018-04-16 19:46:41 -0700

    Update 819. Most Common Word solution (js)

--- HackerRank/001. Warmup/003. Compare The Triplets/README.md
+++ HackerRank/001. Warmup/003. Compare The Triplets/README.md
@@ -1,2 +1,3 @@
 # Compare the Triplets
+
 https://www.hackerrank.com/challenges/compare-the-triplets/problem
--- LeetCode/819. Most Common Word/solution.js
+++ LeetCode/819. Most Common Word/solution.js
@@ -15,6 +15,34 @@ const mostCommonWord = function(paragraph, banned) {
     }
   });
 
+  const wordKeys = Object.keys(wordMap);
+  let result = wordKeys.shift();
+  wordKeys.forEach(key => {
+    if (wordMap[key] > wordMap[result]) {
+      result = key;
+    }
+  });
+
+  return result;
+};
+
+const mostCommonWord2 = function(paragraph, banned) {
+  const bannedMap = {};
+  for (let i = 0; i < banned.length; i++) {
+    const word = banned[i];
+    bannedMap[word] = true;
+  }
+  const words = paragraph.toLowerCase().split(/[!?',;.\s]/g);
+  const l = words.length;
+
+  const wordMap = {};
+  for (let i = 0; i < l; i++) {
+    const word = words[i];
+    if (!bannedMap[word] && word.length) {
+      wordMap[word] = ++wordMap[word] || 1;
+    }
+  }
+
   let resultWord = '';
   let max = 0;
   Object.keys(wordMap).forEach(word => {
