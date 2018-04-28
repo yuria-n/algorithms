7801f51988c82288cabec252582f85793b2c40ea
Julia <berrylily.0709@gmail.com>
2018-04-28 00:27:45 -0700

    Update Plus Minus solution (js)

--- HackerRank/001. Warmup/006. Plus Mins/solution.js
+++ HackerRank/001. Warmup/006. Plus Mins/solution.js
@@ -1,34 +1,32 @@
 'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+// let inputString = '';
+// let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-  inputString += inputStdin;
-});
+// process.stdin.on('data', inputStdin => {
+//   inputString += inputStdin;
+// });
 
-process.stdin.on('end', _ => {
-  inputString = inputString
-    .trim()
-    .split('\n')
-    .map(str => str.trim());
+// process.stdin.on('end', _ => {
+//   inputString = inputString
+//     .trim()
+//     .split('\n')
+//     .map(str => str.trim());
 
-  main();
-});
+//   main();
+// });
 
-function readLine() {
-  return inputString[currentLine++];
-}
+// function readLine() {
+//   return inputString[currentLine++];
+// }
 
 function plusMinus(arr) {
-  const l = arr.length;
-
   const sums = [0, 0, 0];
-  for (let i = 0; i < l; i++) {
-    const a = arr[i];
+
+  arr.forEach(a => {
     if (a > 0) {
       sums[0]++;
     } else if (a < 0) {
@@ -36,21 +34,18 @@ function plusMinus(arr) {
     } else {
       sums[2]++;
     }
-  }
+  });
 
   const p = Math.pow(10, 6);
-  for (let i = 0; i < sums.length; i++) {
-    sums[i] = Math.round(sums[i] / l * p) / p;
-  }
-  return sums;
+  return sums.map(s => Math.round(s / arr.length * p) / p);
 }
 
-function main() {
-  const n = parseInt(readLine(), 10);
+// function main() {
+//   const n = parseInt(readLine(), 10);
 
-  const arr = readLine()
-    .split(' ')
-    .map(arrTemp => parseInt(arrTemp, 10));
+//   const arr = readLine()
+//     .split(' ')
+//     .map(arrTemp => parseInt(arrTemp, 10));
 
-  console.log(plusMinus(arr).join('\n'));
-}
+//   console.log(plusMinus(arr).join('\n'));
+// }
