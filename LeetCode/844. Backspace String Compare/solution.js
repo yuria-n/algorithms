ff3091e370d99bc593dd7e706b184c0fa6d83219
Yuria <nishimura.yuria@gmail.com>
2018-06-05 14:02:42 -0700

    Update 844. Backspace String Compare solution (js)

--- LeetCode/844. Backspace String Compare/solution.js
+++ LeetCode/844. Backspace String Compare/solution.js
@@ -4,36 +4,23 @@
  * @return {boolean}
  */
 const backspaceCompare = function(S, T) {
-  let wordS = '';
-  for (let i = 0; i < S.length; i++) {
-    const c = S[i];
-    if (c !== '#') {
-      wordS += c;
-      continue;
+  return generateWord(S) === generateWord(T);
+
+  function generateWord(str) {
+    let word = '';
+    let count = 0;
+    for (let i = str.length - 1; i >= 0; i--) {
+      const w = str[i];
+      if (w === '#') {
+        count++;
+        continue;
+      }
+      if (count > 0) {
+        count--;
+        continue;
+      }
+      word = w + word;
     }
-    wordS = wordS.substr(0, Math.max(0, wordS.length - 1));
+    return word;
   }
-
-  let wordT = '';
-  for (let i = 0; i < T.length; i++) {
-    const c = T[i];
-    if (c !== '#') {
-      wordT += c;
-      continue;
-    }
-    wordT = wordT.substr(0, Math.max(0, wordT.length - 1));
-  }
-
-  const l = wordS.length;
-  if (l !== wordT.length) {
-    return false;
-  }
-
-  for (let i = 0; i < l; i++) {
-    if (wordS[i] !== wordT[i]) {
-      return false;
-    }
-  }
-
-  return true;
 };
