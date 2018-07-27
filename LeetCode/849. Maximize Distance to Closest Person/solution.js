5c7041c0ee3b4fc546a9ea99e3947d13e6923931
Yuria <nishimura.yuria@gmail.com>
2018-07-26 17:19:13 -0700

    Update 849. Maximize Distance to Closest Person solution

--- LeetCode/849. Maximize Distance to Closest Person/solution.js
+++ LeetCode/849. Maximize Distance to Closest Person/solution.js
@@ -2,50 +2,29 @@
  * @param {number[]} seats
  * @return {number}
  */
-const maxDistToClosest = function(seats) {
-  let start = 0;
-  let end = seats.length;
-  const countLeft = () => {
-    let count = 0;
-    for (let i = 0; i < seats.length; i++) {
-      const n = seats[i];
-      if (n === 1) {
-        start = i + 1;
-        return count;
-      }
-      count++;
-    }
-  };
-  const left = countLeft();
+const maxDistToClosest = seats => {
+  let result = 0;
+  let i = -1;
+  let j = seats.length;
+  let n = 0;
 
-  const countRight = () => {
-    let count = 0;
-    for (let i = seats.length - 1; i >= 0; i--) {
-      const n = seats[i];
-      if (n === 1) {
-        end = i + 1;
-        return count;
-      }
-      count++;
-    }
-  };
-  const right = countRight();
+  while (!seats[++i]) {
+    result = Math.max(result, ++n);
+  }
 
-  const countMiddle = () => {
-    let count = 0;
-    let tmp = 1;
-    for (let i = start; i < end; i++) {
-      const n = seats[i];
-      if (n === 1) {
-        count = Math.max(count, Math.floor(tmp / 2));
-        tmp = 1;
-        continue;
-      }
-      tmp++;
-    }
-    return count;
-  };
-  const middle = countMiddle();
+  n = 0;
+  while (!seats[--j]) {
+    result = Math.max(result, ++n);
+  }
 
-  return Math.max(Math.max(left, right), middle);
+  n = 0;
+  while (i < j) {
+    if (seats[i++]) {
+      n = 0;
+      continue;
+    }
+    result = Math.max(result, Math.floor((++n + 1) / 2));
+  }
+
+  return result;
 };
