06e88613991a96b31de9352eca925d349f56212f
Julia <berrylily.0709@gmail.com>
2018-08-23 21:47:46 -0700

    Update 204. Count Primes solution (js)

--- LeetCode/204. Count Primes/solution.js
+++ LeetCode/204. Count Primes/solution.js
@@ -2,7 +2,22 @@
  * @param {number} n
  * @return {number}
  */
-const countPrimes = function(n) {
+function countPrimes(n) {
+  const nonPrimeMap = Array(n);
+  let count = n < 3 ? 0 : 1;
+  for (let i = 3; i < n; i += 2) {
+    if (nonPrimeMap[i]) {
+      continue;
+    }
+    count++;
+    for (let j = i * i; j < n; j += i) {
+      nonPrimeMap[j] = true;
+    }
+  }
+  return count;
+}
+
+const countPrimes2 = function(n) {
   const nonPrimeMap = {};
   let count = 0;
   for (let i = 2; i < n; i++) {
