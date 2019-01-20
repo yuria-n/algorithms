0eafd815e6cfa969e08e878faef015a68ded6f7d
Yuria <nishimura.yuria@gmail.com>
2019-01-19 23:27:03 -0800

    update 905. Sort Array By Parity solution (js)

--- LeetCode/905. Sort Array By Parity/solution.js
+++ LeetCode/905. Sort Array By Parity/solution.js
@@ -3,9 +3,19 @@
  * @return {number[]}
  */
 const sortArrayByParity = function(A) {
-  const result = [];
+  let i = 0;
+  let j = A.length - 1;
 
-  A.forEach((num) => (num % 2 === 0 ? result.unshift(num) : result.push(num)));
+  while (i !== j) {
+    if (A[i] % 2 === 0) {
+      i++;
+      continue;
+    }
+    let t = A[i];
+    A[i] = A[j];
+    A[j] = t;
+    j--;
+  }
 
-  return result;
+  return A;
 };
