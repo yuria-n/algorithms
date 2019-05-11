1f63e92e50a21bc249e409c850d5fba5c7b0a81d
Yuria <nishimura.yuria@gmail.com>
2019-05-10 22:12:41 -0700

    add another 83. Remove Duplicates from Sorted List solution

--- LeetCode/83. Remove Duplicates from Sorted List/solution.js
+++ LeetCode/83. Remove Duplicates from Sorted List/solution.js
@@ -26,3 +26,34 @@ const deleteDuplicates = function(head) {
   }
   return result;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates2 = function(head) {
+  const result = head;
+  deleteDup(head);
+  return result;
+
+  function deleteDup(node) {
+    if (!node) {
+      return;
+    }
+
+    while (node.next && node.val === node.next.val) {
+      const n = node.next.next;
+      node.next = n;
+      deleteDup(n);
+    }
+
+    return deleteDup(node.next);
+  }
+};
