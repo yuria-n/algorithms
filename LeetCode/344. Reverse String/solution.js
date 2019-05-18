47ea11865842d8e971ce3c8d1ea2e05b6fa4dd25
Yuria <nishimura.yuria@gmail.com>
2019-05-18 16:24:33 -0700

    update 344. Reverse String solution (js)

--- LeetCode/344. Reverse String/solution.js
+++ LeetCode/344. Reverse String/solution.js
@@ -1,7 +1,8 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
  */
+// for loop
 const reverseString = function(s) {
   let result = '';
 
@@ -10,4 +11,32 @@ const reverseString = function(s) {
   }
 
   return result;
+};
+
+// while loop
+const reverseString1 = function(s) {
+  const l = s.length - 1;
+  const half = l / 2;
+
+  let i = -1;
+  while (++i <= half) {
+    const j = l - i;
+    [s[i], s[j]] = [s[j], s[i]];
+  }
+};
+
+// recursion
+const reverseString2 = function(s) {
+  const l = s.length - 1;
+  const half = l / 2;
+  reverse(0);
+
+  function reverse(i) {
+    if (i > half) {
+      return;
+    }
+    const j = l - i;
+    [s[i], s[j]] = [s[j], s[i]];
+    return reverse(i + 1);
+  }
 };
