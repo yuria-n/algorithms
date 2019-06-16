17eac5a340b687f4aa2403ca496c849a8e8cb71f
Yuria <nishimura.yuria@gmail.com>
2019-06-16 14:35:15 -0700

    Update 217. Contains Duplicate solution (js)

--- LeetCode/217. Contains Duplicate/solution.js
+++ LeetCode/217. Contains Duplicate/solution.js
@@ -13,3 +13,19 @@ const containsDuplicate = function(nums) {
   }
   return false;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate2 = function(nums) {
+  const set = new Set();
+  for (const n of nums) {
+    if (set.has(n)) {
+      return true;
+    }
+    set.add(n);
+  }
+
+  return false;
+};
