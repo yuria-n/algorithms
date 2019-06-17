8df13c63fca7af0446628c62fb84b0005e8098ef
Yuria <nishimura.yuria@gmail.com>
2019-06-16 17:38:56 -0700

    Update 283. Move Zeroes solution (js)

--- LeetCode/283. Move Zeroes/solution.js
+++ LeetCode/283. Move Zeroes/solution.js
@@ -3,15 +3,26 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const moveZeroes = function(nums) {
-  const l = nums.length;
   let p = 0;
-  for (let i = 0; i < l; i++) {
-    const n = nums[i];
+  nums.forEach((n) => {
     if (n) {
       nums[p++] = n;
     }
-  }
-  while (p < l) {
+  });
+
+  while (p < nums.length) {
     nums[p++] = 0;
+  }
+};
+
+const moveZeroes2 = function(nums) {
+  let p = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (!n) {
+      continue;
+    }
+    [nums[p], nums[i]] = [n, nums[p]];
+    p++;
   }
 };
