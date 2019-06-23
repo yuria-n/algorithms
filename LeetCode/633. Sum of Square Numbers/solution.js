438376df7d961436b9f6d623e83cad6fdd04467c
Yuria <nishimura.yuria@gmail.com>
2019-06-23 12:06:15 -0700

    Update 633. Sum of Square Numbers (js)

--- LeetCode/633. Sum of Square Numbers/solution.js
+++ LeetCode/633. Sum of Square Numbers/solution.js
@@ -2,14 +2,22 @@
  * @param {number} c
  * @return {boolean}
  */
-
-// set
-judgeSquareSum = function(c) {
+const judgeSquareSum = function(c) {
   const set = new Set();
   for (let i = 0; i <= Math.sqrt(c); i++) {
     const s = i * i;
     set.add(s);
     if (set.has(c - s)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const judgeSquareSum2 = function(c) {
+  for (let i = 0; i <= Math.floor(Math.sqrt(c)); i++) {
+    let a = Math.sqrt(c - i * i);
+    if (a === Math.floor(a)) {
       return true;
     }
   }
