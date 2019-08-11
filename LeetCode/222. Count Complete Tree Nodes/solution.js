0a289c4478638613d0a59b32dc3395278ed7eebd
Yuria <nishimura.yuria@gmail.com>
2019-08-11 16:57:03 -0700

    Update 222. Count Complete Tree Nodes solution (js)

--- LeetCode/222. Count Complete Tree Nodes/solution.js
+++ LeetCode/222. Count Complete Tree Nodes/solution.js
@@ -13,10 +13,35 @@ const countNodes = function(root) {
   if (!root) {
     return 0;
   }
+
+  let sumL = 0;
+  let sumR = 0;
+  let nodeL = root;
+  let nodeR = root;
+
+  while (nodeL) {
+    sumL++;
+    nodeL = nodeL.left;
+  }
+  while (nodeR) {
+    sumR++;
+    nodeR = nodeR.right;
+  }
+  if (sumL === sumR) {
+    return 2 ** sumL - 1;
+  }
+
   return countNodes(root.left) + countNodes(root.right) + 1;
 };
 
 const countNodes2 = function(root) {
+  if (!root) {
+    return 0;
+  }
+  return countNodes2(root.left) + countNodes2(root.right) + 1;
+};
+
+const countNodes3 = function(root) {
   let result = 0;
   count(root);
   return result;
