70a8bbceb2bd52deb42780325befa93add48e912
Yuria <nishimura.yuria@gmail.com>
2019-10-05 15:18:58 -0700

    Update 832. Flipping an Image solution (js)

--- LeetCode/832. Flipping an Image/solution.js
+++ LeetCode/832. Flipping an Image/solution.js
@@ -3,17 +3,12 @@
  * @return {number[][]}
  */
 const flipAndInvertImage = function(A) {
-  for (let i = 0; i < A.length; i++) {
-    const row = A[i];
+  for (const row of A) {
     const l = row.length;
-    for (let j = 0; j < Math.ceil(l / 2); j++) {
+    for (let j = 0; j < l / 2; j++) {
       const k = l - j - 1;
-      [A[i][j], A[i][k]] = [invert(row[k]), invert(row[j])];
+      [row[j], row[k]] = [row[k] ^ 1, row[j] ^ 1];
     }
   }
   return A;
-
-  function invert(n) {
-    return n === 1 ? 0 : 1;
-  }
 };
