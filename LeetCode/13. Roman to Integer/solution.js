1cb75317a78ca531f1b45e4ef63edb842e95c208
Yuria <nishimura.yuria@gmail.com>
2019-11-20 21:15:06 -0800

    update 13. Roman to Integer solution (js)

--- LeetCode/13. Roman to Integer/solution.js
+++ LeetCode/13. Roman to Integer/solution.js
@@ -5,7 +5,7 @@
 const romanToInt = function(s) {
   let sum = 0;
   for (let i = s.length - 1; i >= 0; i--) {
-    const prev = s[i + 1] || '';
+    const p = s[i + 1];
     switch (s[i]) {
       case 'M':
         sum += 1000;
@@ -14,23 +14,42 @@ const romanToInt = function(s) {
         sum += 500;
         break;
       case 'C':
-        sum += prev === 'D' || prev === 'M' ? -100 : 100;
+        sum += p === 'D' || p === 'M' ? -100 : 100;
         break;
       case 'L':
         sum += 50;
         break;
       case 'X':
-        sum += prev === 'L' || prev === 'C' ? -10 : 10;
+        sum += p === 'L' || p === 'C' ? -10 : 10;
         break;
       case 'V':
         sum += 5;
         break;
       case 'I':
-        sum += prev === 'V' || prev === 'X' ? -1 : 1;
+        sum += p === 'V' || p === 'X' ? -1 : 1;
         break;
       default:
         break;
     }
+  }
+  return sum;
+};
+
+const map = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1,
+};
+const romanToInt = function(s) {
+  let sum = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    const m = map[s[i + 1]];
+    const n = map[s[i]];
+    sum += n < m ? -n : n;
   }
   return sum;
 };
