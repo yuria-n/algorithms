cc6fbfc112839f0946ddc669c0bc8dceff36e92e
Yuria <nishimura.yuria@gmail.com>
2019-11-24 20:45:34 -0800

    update 1267. Count Servers that Communicate solution (js)

--- LeetCode/1267. Count Servers that Communicate/solution.js
+++ LeetCode/1267. Count Servers that Communicate/solution.js
@@ -4,33 +4,20 @@
  */
 const countServers = function(grid) {
   let result = 0;
-
-  // Count all computers
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j]) {
-        result++;
-      }
-    }
-  }
-
-  // Subtract isolated computers
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (!grid[i][j]) {
+      if (
+        !grid[i][j] ||
+        (helper(i - 1, j, -1, 0) &&
+          helper(i + 1, j, 1, 0) &&
+          helper(i, j - 1, 0, -1) &&
+          helper(i, j + 1, 0, 1))
+      ) {
         continue;
       }
-      if (
-        helper(i - 1, j, -1, 0) &&
-        helper(i + 1, j, 1, 0) &&
-        helper(i, j - 1, 0, -1) &&
-        helper(i, j + 1, 0, 1)
-      ) {
-        result--;
-      }
+      result++;
     }
   }
-
   return result;
 
   function helper(x, y, dx, dy) {
