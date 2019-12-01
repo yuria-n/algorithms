8725ac798e3119b39c8fb35edde76c316b15f36d
Yuria <nishimura.yuria@gmail.com>
2019-11-30 16:04:00 -0800

    update 168. Excel Sheet Column Title solution (js)

--- LeetCode/168. Excel Sheet Column Title/solution.js
+++ LeetCode/168. Excel Sheet Column Title/solution.js
@@ -7,13 +7,27 @@ const digits = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const length = digits.length;
 
 const convertToTitle = function(n) {
-    let result = '';
+  let result = '';
 
-    while (n > 0) {
-        const p = n % length;
-        result = digits.charAt((!!p ? p - 1 : length - 1)) + result;
-        n = Math.floor((n - 1) / length);
-    }
+  while (n > 0) {
+    const p = n % length;
+    result = digits.charAt(!!p ? p - 1 : length - 1) + result;
+    n = Math.floor((n - 1) / length);
+  }
 
-    return result;
+  return result;
+};
+
+// charCode: 65 - 90
+const BASE = 26;
+const CODE = 64;
+const convertToTitle = function(n) {
+  let result = '';
+  while (n > 0) {
+    const remainder = n % BASE;
+    const i = !remainder ? BASE : remainder;
+    result = String.fromCharCode(i + CODE) + result;
+    n = (n - i) / BASE;
+  }
+  return result;
 };
