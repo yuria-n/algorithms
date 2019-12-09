50513b7d5ee4e537bf059db0d04a0c54e6813500
Yuria <nishimura.yuria@gmail.com>
2019-12-08 22:00:53 -0800

    Update 1282. Group the People Given the Group Size They Belong To solution (js)

--- LeetCode/1282. Group the People Given the Group Size They Belong To/solution.js
+++ LeetCode/1282. Group the People Given the Group Size They Belong To/solution.js
@@ -3,22 +3,16 @@
  * @return {number[][]}
  */
 const groupThePeople = function(groupSizes) {
-  const map = new Map();
-  for (let i = 0; i < groupSizes.length; i++) {
-    const size = groupSizes[i];
-    const arr = map.get(size) || [];
-    map.set(size, [...arr, i]);
-  }
   const result = [];
-  for (const [key, arr] of map.entries()) {
-    const l = arr.length;
-    if (key === l) {
+  const map = new Map();
+  for (const [i, size] of groupSizes.entries()) {
+    const arr = map.get(size) || [];
+    if (arr.push(i) === size) {
       result.push(arr);
+      map.delete(size);
       continue;
     }
-    for (let i = 0; i < arr.length; i += key) {
-      result.push(arr.slice(i, i + key));
-    }
+    map.set(size, arr);
   }
   return result;
 };
