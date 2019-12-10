2e405956816bc455c279b21f97d5e0654f8f940a
Yuria <nishimura.yuria@gmail.com>
2019-12-09 23:40:46 -0800

    Update 409.Longest Palindrome solution (js)

--- LeetCode/409. Longest Palindrome/solution.js
+++ LeetCode/409. Longest Palindrome/solution.js
@@ -4,15 +4,14 @@
  */
 const longestPalindrome = function(s) {
   let count = 0;
-  const map = new Map();
-  for (const char of s) {
-    const cur = map.get(char);
-    if (!cur) {
-      map.set(char, 1);
+  const set = new Set();
+  for (const c of s) {
+    if (!set.has(c)) {
+      set.add(c);
       continue;
     }
     count += 2;
-    map.delete(char);
+    set.delete(c);
   }
-  return count + (!map.size ? 0 : 1);
+  return count + (!set.size ? 0 : 1);
 };
