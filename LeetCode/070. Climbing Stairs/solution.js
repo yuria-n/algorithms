eb6d6dcf845e482063eefb2f8eb761429073d2a1
Yuria <nishimura.yuria@gmail.com>
2019-12-21 16:18:25 -0800

    update 70. Climbing Stairs solution (js)

--- LeetCode/070. Climbing Stairs/solution.js
+++ LeetCode/070. Climbing Stairs/solution.js
@@ -2,12 +2,38 @@
  * @param {number} n
  * @return {number}
  */
+
+// Top down DP
 const climbStairs = function(n) {
-  const steps = [0, 1, 2];
+  const map = new Map();
+  return helper(n);
 
-  for (let i = 3; i <= n; i++) {
-    steps[i] = steps[i - 2] + steps[i - 1];
+  function helper(i) {
+    if (map.has(i)) {
+      return map.get(i);
+    }
+    const val = i <= 1 ? 1 : helper(i - 1) + helper(i - 2);
+    map.set(i, val);
+    return val;
   }
+};
 
-  return steps[n];
+// Bottom up DP
+const climbStairs = function(n) {
+  let curr = 1;
+  let prev = 1;
+  for (let i = 0; i <= n; i++) {
+    const val = curr + prev;
+    prev = curr;
+    curr = val;
+  }
+  return curr;
+};
+
+const climbStairs = function(n) {
+  const dp = [];
+  for (let i = 0; i <= n; i++) {
+    dp[i] = i <= 1 ? 1 : dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
 };
