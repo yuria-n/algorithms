51270a441349ff6d3b6866c09abd1d13ca2b6f06
Yuria <nishimura.yuria@gmail.com>
2020-01-05 11:08:23 +0900

    update 1304. Find N Unique Integers Sum up to Zero solution (js)

--- LeetCode/1304. Find N Unique Integers Sum up to Zero/solution.js
+++ LeetCode/1304. Find N Unique Integers Sum up to Zero/solution.js
@@ -7,9 +7,8 @@ const sumZero = function(n) {
   if (n % 2) {
     result.push(0);
   }
-  for (let i = 1; i <= Math.floor(n / 2); i++) {
-    result.push(i);
-    result.push(-i);
+  for (let i = 1; i <= n / 2; i++) {
+    result.push(i, -i);
   }
   return result;
 };
