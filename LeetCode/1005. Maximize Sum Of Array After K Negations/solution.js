73ac39df4c322ec29ae1a9c7da5a1397d53927cf
Yuria <nishimura.yuria@gmail.com>
2020-01-25 17:54:25 -0800

    Update 1005. Maximize Sum Of Array After K Negations (js)

--- LeetCode/1005. Maximize Sum Of Array After K Negations/solution.js
+++ LeetCode/1005. Maximize Sum Of Array After K Negations/solution.js
@@ -12,18 +12,10 @@ const largestSumAfterKNegations = function(A, K) {
       K--;
     }
   }
-  A.sort((n1, n2) => n1 - n2);
   if (K % 2) {
-    A[0] = -A[0];
+    const min = Math.min(...A);
+    const i = A.findIndex(n => n === min);
+    A[i] = -A[i];
   }
   return A.reduce((n, sum) => (n += sum));
 };
-
-// -1, -4, -2, -3, 1, 2, 3
-// 4
-// 3
-// 6
-// 昇順でソートする -4, -3, -2, -1, 0, 1, 2, 3
-// もしマイナス部分の数がKより小さい場合、すべてを変換する。そうでない場合、小さいもの順に変える 4, 3, 2, 1, 0, 1, 2, 3
-// もしKが余った場合（あまりをmとする）、mが奇数の場合、最小値を変える、そうでない場合そのまま
-// sumして返す
