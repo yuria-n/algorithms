4523783b988c64f776f8203303056d34e6c241f2
Yuria <nishimura.yuria@gmail.com>
2020-03-31 23:24:28 -0700

    update 345. Reverse Vowels of a String solution (js)

--- LeetCode/345. Reverse Vowels of a String/solution.js
+++ LeetCode/345. Reverse Vowels of a String/solution.js
@@ -1,33 +1,47 @@
+const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
 /**
  * @param {string} s
  * @return {string}
  */
-const vowels = {
-  a: true,
-  e: true,
-  i: true,
-  o: true,
-  u: true,
-  A: true,
-  E: true,
-  I: true,
-  O: true,
-  U: true,
-};
-
 const reverseVowels = function(s) {
-  let result = '';
-  const stacks = [];
   const l = s.length;
+  const indices = [];
   for (let i = 0; i < l; i++) {
-    const c = s[i];
-    if (vowels[c]) {
-      stacks.push(c);
+    if (vowels.has(s[i])) {
+      indices.push(i);
     }
   }
+  let result = '';
   for (let i = 0; i < l; i++) {
-    const c = s[i];
-    result += vowels[c] ? stacks.pop() : c;
+    const k = vowels.has(s[i]) ? indices.pop() : i;
+    result += s[k];
   }
   return result;
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseVowels = function(s) {
+  const regex = /[aeiou]/i;
+  let prefix = '';
+  let suffix = '';
+  let i = -1;
+  let j = s.length;
+  while (++i < --j) {
+    if (!regex.test(s[i])) {
+      prefix += s[i];
+      j++;
+      continue;
+    }
+    if (!regex.test(s[j])) {
+      suffix = s[j] + suffix;
+      i--;
+      continue;
+    }
+    prefix += s[j];
+    suffix = s[i] + suffix;
+  }
+  return prefix + (i === j ? s[i] : '') + suffix;
 };
