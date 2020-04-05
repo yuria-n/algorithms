30560c1d98337d95a522f54ee9862e27b3697825
Yuria <nishimura.yuria@gmail.com>
2020-04-05 16:46:21 -0700

    update 122. Best Time to Buy and Sell Stock II solution (js)

--- LeetCode/122. Best Time to Buy and Sell Stock II/solution.js
+++ LeetCode/122. Best Time to Buy and Sell Stock II/solution.js
@@ -2,13 +2,11 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function(prices) {
+const maxProfit = function (prices) {
   let result = 0;
   for (let i = 0; i < prices.length - 1; i++) {
     const diff = prices[i + 1] - prices[i];
-    if (diff > 0) {
-      result += diff;
-    }
+    result += diff <= 0 ? 0 : diff;
   }
   return result;
 };
