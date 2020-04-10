8a282332388bc76c21a57a206356d92ccde3ff3c
Yuria <nishimura.yuria@gmail.com>
2020-04-10 12:46:57 -0700

    update 155. Min Stack solution (js)

--- LeetCode/155. Min Stack/solution.js
+++ LeetCode/155. Min Stack/solution.js
@@ -3,46 +3,88 @@
  */
 class MinStack {
   constructor() {
+    this.min = Infinity;
     this.stack = [];
   }
-
   /**
    * @param {number} x
    * @return {void}
    */
-  push(x) {
+  push = (x) => {
     this.stack.push(x);
-  }
-
+    if (x < this.min) {
+      this.min = x;
+    }
+  };
   /**
    * @return {void}
    */
-  pop() {
-    this.stack.pop();
-  }
-
-  /**
-   * @return {number}
-   */
-  top() {
-    return this.stack[this.stack.length - 1];
-  }
-
-  /**
-   * @return {number}
-   */
-  getMin() {
-    let min = Infinity;
-    for (const num of this.stack) {
-      min = Math.min(min, num);
+  pop = () => {
+    const x = this.stack.pop();
+    if (x === this.min) {
+      this.min = Math.min(...this.stack);
     }
-    return min;
-  }
+  };
+  /**
+   * @return {number}
+   */
+  top = () => {
+    return this.stack[this.stack.length - 1];
+  };
+  /**
+   * @return {number}
+   */
+  getMin = () => {
+    return this.min;
+  };
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * var obj = Object.create(MinStack).createNew()
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+/**
+ * initialize your data structure here.
+ */
+class MinStack {
+  constructor() {
+    this.stack = [];
+  }
+  /**
+   * @param {number} x
+   * @return {void}
+   */
+  push = (x) => {
+    this.stack.push(x);
+  };
+  /**
+   * @return {void}
+   */
+  pop = () => {
+    this.stack.pop();
+  };
+  /**
+   * @return {number}
+   */
+  top = () => {
+    return this.stack[this.stack.length - 1];
+  };
+  /**
+   * @return {number}
+   */
+  getMin = () => {
+    return Math.min(...this.stack);
+  };
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
  * obj.push(x)
  * obj.pop()
  * var param_3 = obj.top()
