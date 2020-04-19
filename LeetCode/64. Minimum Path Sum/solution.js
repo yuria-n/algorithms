c8d4dfe4850f3fc4d2db6527cf0e020cbbe997cc
Yuria <nishimura.yuria@gmail.com>
2020-04-18 18:25:47 -0700

    update 64. Minimum Path Sum solution (js)

--- LeetCode/64. Minimum Path Sum/solution.js
+++ LeetCode/64. Minimum Path Sum/solution.js
@@ -5,17 +5,15 @@
 const minPathSum = function (grid) {
   const row = grid.length;
   const col = grid[0].length;
-  const dp = Array.from({ length: row }, () => new Array(col).fill(0));
   for (let y = 0; y < row; y++) {
     for (let x = 0; x < col; x++) {
       if (!y && !x) {
-        dp[y][x] = grid[y][x];
         continue;
       }
-      const left = !x ? Infinity : dp[y][x - 1];
-      const top = !y ? Infinity : dp[y - 1][x];
-      dp[y][x] = grid[y][x] + Math.min(left, top);
+      const left = !x ? Infinity : grid[y][x - 1];
+      const top = !y ? Infinity : grid[y - 1][x];
+      grid[y][x] += Math.min(left, top);
     }
   }
-  return dp[row - 1][col - 1];
+  return grid[row - 1][col - 1];
 };
