ac032f74400215950d5d068a8744b0a7a8aa12d7
Yuria <nishimura.yuria@gmail.com>
2020-05-03 13:43:27 -0700

    add 383. Ransom Note solution (js)

--- LeetCode/383. Ransom Note/solution.js
+++ LeetCode/383. Ransom Note/solution.js
@@ -3,19 +3,18 @@
  * @param {string} magazine
  * @return {boolean}
  */
-const canConstruct = function(ransomNote, magazine) {
-    const map = {};
-    for (let i = 0; i < magazine.length; i++) {
-        const key = magazine[i];
-        map[key] = map[key] ? map[key] + 1 : 1;
-        // map[key] = ++map[key] || 1;
+const canConstruct = function (ransomNote, magazine) {
+  const map = new Map();
+  for (const c of magazine) {
+    const count = map.get(c) || 0;
+    map.set(c, count + 1);
+  }
+  for (const c of ransomNote) {
+    const count = map.get(c) || 0;
+    if (!count) {
+      return false;
     }
-    
-    for (let i = 0; i < ransomNote.length; i++) {
-        const key = ransomNote[i];
-        if (!map[key]--) {
-            return false;
-        }
-    }
-    return true;
+    map.set(c, count - 1);
+  }
+  return true;
 };
