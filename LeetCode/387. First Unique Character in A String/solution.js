8d33637c19a55bece37d43cf475d52333f0c15fa
Yuria <nishimura.yuria@gmail.com>
2020-05-06 11:59:59 -0700

    update 387. First Unique Character in A String solution (js)

--- LeetCode/387. First Unique Character in A String/solution.js
+++ LeetCode/387. First Unique Character in A String/solution.js
@@ -2,19 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-const firstUniqChar = function(s) {
-    const l = s.length;
-    const map = {};
-    for (let i = 0; i < l; i++) {
-        const c = s[i];
-        map[c] = ++map[c] || 1;
+const firstUniqChar = function (s) {
+  const arr = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    arr[s[i].charCodeAt() - 97]++;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (arr[s[i].charCodeAt() - 97] === 1) {
+      return i;
     }
-    
-    for (let i = 0; i < l; i++) {
-        if (map[s[i]] === 1) {
-            return i;
-        }
+  }
+  return -1;
+};
+
+const firstUniqChar = function (s) {
+  const l = s.length;
+  const map = {};
+  for (let i = 0; i < l; i++) {
+    const c = s[i];
+    map[c] = ++map[c] || 1;
+  }
+  for (let i = 0; i < l; i++) {
+    if (map[s[i]] === 1) {
+      return i;
     }
-    
-    return -1;
+  }
+  return -1;
 };
