d73bff54dfbe759695f93764104d0e4670a4cf6d
Yuria <nishimura.yuria@gmail.com>
2023-05-24 20:50:54 -0700

    Update 141. Linked List Cycle solution (js)

--- LeetCode/141. Linked List Cycle/solution.js
+++ LeetCode/141. Linked List Cycle/solution.js
@@ -11,31 +11,28 @@
  * @return {boolean}
  */
 // O(n) space
-const hasCycle = function(head) {
-  const map = new Map();
-  while (head) {
-    if (map.has(head)) {
+function hasCycle(head) {
+  const visited = new Set();
+  while(head !== null) {
+    if (visited.has(head)) {
       return true;
     }
-    map.set(head, true);
+    visited.add(head);
     head = head.next;
   }
   return false;
 };
 
 // O(1) space
-const hasCycle2 = function(head) {
-  if (!head) {
-    return false;
-  }
+function hasCycle2(head) {
   let slow = head;
-  let fast = head;
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
+  let fast = head?.next;
+  while(fast !== null && fast !== undefined) {
     if (slow === fast) {
       return true;
     }
+    slow = slow.next;
+    fast = fast.next?.next;
   }
   return false;
 };
