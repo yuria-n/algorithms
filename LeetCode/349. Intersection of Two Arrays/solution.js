f91deeb3d392b3a4abe8f484fd50fe7a8e2df5aa
Yuria <nishimura.yuria@gmail.com>
2023-05-24 22:11:34 -0700

    Update 349. Intersection of Two Arrays solution (js)

--- LeetCode/349. Intersection of Two Arrays/solution.js
+++ LeetCode/349. Intersection of Two Arrays/solution.js
@@ -3,24 +3,23 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-const intersection = function(nums1, nums2) {
+function intersection(nums1, nums2) {
   const result = [];
   const set1 = new Set(nums1);
   const set2 = new Set(nums2);
-  set1.forEach((n) => {
+  for (const n of set1) {
     if (set2.has(n)) {
       result.push(n);
     }
-  });
-
+  }
   return result;
-};
+}
 
-const intersection = function(nums1, nums2) {
-  const set1 = new Set(nums1);
-  return [...new Set(nums2.filter((n) => set1.has(n)))];
-};
-
-const intersection = function(nums1, nums2) {
+function intersection2(nums1, nums2) {
   return [...new Set(nums1.filter((n) => nums2.includes(n)))];
-};
+}
+
+function intersection3(nums1, nums2) {
+  const set1 = new Set(nums1);
+  return Array.from(new Set(nums2)).filter((n) => set1.has(n));
+}
