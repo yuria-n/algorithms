b95dc0c30da31a14bc6dca3903b37c8bcbafb415
Yuria <nishimura.yuria@gmail.com>
2023-05-24 22:28:18 -0700

    Update 929. Unique Email Addresses solution (js)

--- LeetCode/929. Unique Email Addresses/solution.js
+++ LeetCode/929. Unique Email Addresses/solution.js
@@ -2,14 +2,37 @@
  * @param {string[]} emails
  * @return {number}
  */
-const numUniqueEmails = function(emails) {
-    const map = {};
+function numUniqueEmails(emails) {
+  const map = {};
 
-    emails.forEach((email) => {
-        const [, local, dmain] = email.match(/(.*@)(.*)/);
-        const newEmail = `${local.replace(/\./g, '').replace(/\+.*@/, '@')}${dmain}`;
-        map[newEmail] = 1;
-    });
+  emails.forEach((email) => {
+    const [, local, domain] = email.match(/(.*@)(.*)/);
+    const newEmail = `${local
+      .replace(/\./g, "")
+      .replace(/\+.*@/, "@")}${domain}`;
+    map[newEmail] = 1;
+  });
 
-    return Object.keys(map).length;
-};
+  return Object.keys(map).length;
+}
+
+function numUniqueEmails2(emails) {
+  const result = new Set();
+  for (const email of emails) {
+    const [localName, domainName] = email.split("@");
+    const [local] = localName.split("+");
+    result.add(`${local.replaceAll(".", "")}@${domainName}`);
+  }
+  return result.size;
+}
+
+function numUniqueEmails3(emails) {
+  const set = new Set();
+
+  for (const email of emails) {
+    const [, local, , domain] = email.match(/([a-z.]+)(\+*.*@)(.+)/);
+    set.add(`${local.replaceAll(".", "")}@${domain}`);
+  }
+
+  return set.size;
+}
