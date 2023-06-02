09f70f5e03ac2bf71727f319abf8c22dd8138df8
Yuria <nishimura.yuria@gmail.com>
2023-06-01 22:15:19 -0700

    Update 35. Search Insert Position solution (js)

--- LeetCode/35. Search insert Position/README.md
+++ LeetCode/35. Search insert Position/README.md
@@ -1,29 +1,42 @@
 # 35. Search Insert Position
 
-Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+https://leetcode.com/problems/search-insert-position/description/
 
-You may assume no duplicates in the array.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-Example 1:
+You must write an algorithm with `O(log n)` runtime complexity.
+
+**Example 1:**
+
 ```
 Input: [1,3,5,6], 5
 Output: 2
 ```
 
-Example 2:
+**Example 2:**
+
 ```
 Input: [1,3,5,6], 2
 Output: 1
 ```
 
-Example 3:
+**Example 3:**
+
 ```
 Input: [1,3,5,6], 7
 Output: 4
 ```
 
-Example 4:
+**Example 4:**
+
 ```
 Input: [1,3,5,6], 0
 Output: 0
 ```
+
+**Constraints:**
+
+- `1 <= nums.length <= 104`
+- `-104 <= nums[i] <= 104`
+- `nums` contains **distinct** values sorted in **ascending** order.
+- `-104 <= target <= 104`
--- LeetCode/35. Search insert Position/solution.js
+++ LeetCode/35. Search insert Position/solution.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number}
  */
-const searchInsert = function(nums, target) {
+const searchInsert = function (nums, target) {
   let i = 0;
   while (nums[i] < target) {
     i++;
@@ -16,13 +16,33 @@ const searchInsert = function(nums, target) {
  * @param {number} target
  * @return {number}
  */
-const searchInsert2 = function(nums, target) {
+const searchInsert2 = function (nums, target) {
   const l = nums.length;
   for (let i = 0; i < l; i++) {
     if (nums[i] >= target) {
       return i;
     }
   }
-
   return l;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function searchInsert3(nums, target) {
+  return findIndex(0, nums.length);
+
+  function findIndex(left, right) {
+    if (left === right) {
+      return left;
+    }
+    const mid = Math.floor((left + right) / 2);
+    const n = nums[mid];
+    if (n === target) {
+      return mid;
+    }
+    return n > target ? findIndex(left, mid) : findIndex(mid + 1, right);
+  }
+}
