ae5b50b9b9c4e4d2ed73ab7cb32d02753286a096
Yuria <nishimura.yuria@gmail.com>
2023-06-21 20:44:07 -0700

    Update 993. Cousins in Binary Tree solution (js)

--- LeetCode/993. Cousins in Binary Tree/solution.js
+++ LeetCode/993. Cousins in Binary Tree/solution.js
@@ -11,7 +11,7 @@
  * @param {number} y
  * @return {boolean}
  */
-const isCousins = function (root, x, y) {
+function isCousins(root, x, y) {
   const [xParent, xDepth] = dfs(root, root.val, x, 0);
   const [yParent, yDepth] = dfs(root, root.val, y, 0);
   return xDepth === yDepth && xParent !== yParent;
@@ -24,7 +24,41 @@ const isCousins = function (root, x, y) {
       return [parent, depth + 1];
     }
     return (
-      dfs(node.left, node.val, target, depth + 1) || dfs(node.right, node.val, target, depth + 1)
+      dfs(node.left, node.val, target, depth + 1) ||
+      dfs(node.right, node.val, target, depth + 1)
     );
   }
-};
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+function isCousins2(root, x, y) {
+  let [lvx, pvx] = findVal(root, root.val, 0, x);
+  let [lvy, pvy] = findVal(root, root.val, 0, y);
+  return lvx === lvy && pvx !== pvy;
+
+  function findVal(node, prev, lv, val) {
+    while (node !== null) {
+      if (node.val === val) {
+        return [lv, prev];
+      }
+      return (
+        findVal(node.left, node.val, lv + 1, val) ??
+        findVal(node.right, node.val, lv + 1, val)
+      );
+    }
+    return null;
+  }
+}
