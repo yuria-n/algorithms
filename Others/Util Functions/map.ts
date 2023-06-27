0edce8f7b1b9ddd838791ca184097e3720670611
Yuria <nishimura.yuria@gmail.com>
2023-06-26 22:08:08 -0700

    Update map (ts)

--- Others/Util Functions/map.ts
+++ Others/Util Functions/map.ts
@@ -5,7 +5,7 @@ type Iterator<T, V> = (val: T, index: number, arr: T[]) => V;
 function map<T, V>(arr: T[], iterator: Iterator<T, V>): V[] {
   const output: V[] = new Array(arr.length);
   for (let index = 0; index < arr.length; index++) {
-    output.push(iterator(arr[index], index, arr));
+    output[index] = iterator(arr[index], index, arr);
   }
   return output;
 }
