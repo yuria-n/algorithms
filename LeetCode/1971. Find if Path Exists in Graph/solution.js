e823d21f25f6020ee0be6538b7ab681bcaa65a28
Yuria <nishimura.yuria@gmail.com>
2023-07-04 21:40:52 -0700

    Update 1971. Find if Path Exists in Graph solution (js)

--- LeetCode/1971. Find if Path Exists in Graph/solution.js
+++ LeetCode/1971. Find if Path Exists in Graph/solution.js
@@ -7,27 +7,27 @@
  */
 function validPath(n, edges, source, destination) {
   const map = new Map();
-  for (let i = 0; i < n; i++) {
-    map.set(i, new Set([i]));
-  }
-  for (const [u, v] of edges) {
-    map.get(u).add(v);
-    map.get(v).add(u);
-  }
-  return validateEdge(destination, map.get(destination), new Set());
 
-  function validateEdge(n, set, visited) {
-    if (n === source) {
+  for (const [u, v] of edges) {
+    pushEdge(u, v);
+    pushEdge(v, u);
+  }
+
+  return validateEdge(source, new Set());
+
+  function pushEdge(a, b) {
+    if (!map.has(a)) {
+      map.set(a, []);
+    }
+    map.get(a).push(b);
+  }
+
+  function validateEdge(n, visited) {
+    if (n === destination) {
       return true;
     }
 
     visited.add(n);
-    for (const m of set) {
-      if (!visited.has(m) && validateEdge(m, map.get(m), visited)) {
-        return true;
-      }
-    }
-
-    return false;
+    return map.get(n).some((m) => !visited.has(m) && validateEdge(m, visited));
   }
 }
