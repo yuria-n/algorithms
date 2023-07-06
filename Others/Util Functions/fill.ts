0087360478baaba9dafcdcdd207f4416934ec043
Yuria <nishimura.yuria@gmail.com>
2023-07-06 15:54:44 -0700

    Update fill

--- Others/Util Functions/fill.ts
+++ Others/Util Functions/fill.ts
@@ -1,10 +1,8 @@
 // _.fill
 
 function fill<T>(arr: T[], val: T, start = 0, end = arr.length): T[] {
-  for (let i = 0; i < arr.length; i++) {
-    if (i >= start && i < end) {
-      arr[i] = val;
-    }
+  for (let i = start; i < Math.min(end, arr.length); i++) {
+    arr[i] = val;
   }
   return arr;
 }
