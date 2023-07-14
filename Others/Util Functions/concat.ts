aa676fe61e36696a0e1c28fbf816fb69be563999
Yuria <nishimura.yuria@gmail.com>
2023-07-13 19:53:44 -0700

    Update concat

--- Others/Util Functions/concat.ts
+++ Others/Util Functions/concat.ts
@@ -3,7 +3,11 @@
 function concat<T>(arr: T[], ...vals: T[]): T[] {
   const result = [...arr];
   for (const val of vals) {
-    result.push(val);
+    if (!Array.isArray(val)) {
+      result.push(val);
+      continue;
+    }
+    result.push(...val);
   }
   return result;
 }
