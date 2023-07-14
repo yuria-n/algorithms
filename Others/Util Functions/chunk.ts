0c73931353ced3699678f9acbbe7ba07178af71b
Yuria <nishimura.yuria@gmail.com>
2023-07-13 20:06:21 -0700

    Update chunk

--- Others/Util Functions/chunk.ts
+++ Others/Util Functions/chunk.ts
@@ -1,9 +1,8 @@
 // _.chunk
 
 function chunk<T>(arr: T[], size = 1): T[][] {
-  const result = [];
-  for (let i = 0; i < arr.length; i += size) {
-    result.push(arr.slice(i, Math.min(i + size, arr.length)));
-  }
-  return result;
+  return Array.from({ length: Math.ceil(arr.length / size) }, (_, index) => {
+    const i = size * index;
+    return arr.slice(i, i + size);
+  });
 }
