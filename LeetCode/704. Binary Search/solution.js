0c7bd8cfe7ac292ea5a958f0c6ea488a9463a182
Yuria <nishimura.yuria@gmail.com>
2024-01-17 16:52:26 -0800

    Update 704. Binary Search solution (js)

--- LeetCode/704. Binary Search/solution.js
+++ LeetCode/704. Binary Search/solution.js
@@ -3,24 +3,20 @@
  * @param {number} target
  * @return {number}
  */
-const search = function(nums, target) {
+function search(nums, target) {
   let left = 0;
   let right = nums.length - 1;
-
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
     const n = nums[mid];
-
     if (n === target) {
       return mid;
     }
-
     if (n < target) {
       left = mid + 1;
       continue;
     }
     right = mid - 1;
   }
-
   return -1;
-};
+}
