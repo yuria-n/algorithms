3fc4bb1ef6abde2a504828745f69c29caf1a4087
Yuria <nishimura.yuria@gmail.com>
2024-04-17 13:05:39 -0700

    Update 1448. Count Good Nodes in Binary Tree solution (js)

--- LeetCode/1448. Count Good Nodes in Binary Tree/solution.js
+++ LeetCode/1448. Count Good Nodes in Binary Tree/solution.js
@@ -27,3 +27,34 @@ function goodNodes(root) {
     dfs(node.right, max);
   }
 }
+
+// BFS
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function goodNodes2(root) {
+  let count = 0;
+  const stack = [[root, root.val]];
+  while (stack.length > 0) {
+    const [node, max] = stack.pop();
+    if (node === null) {
+      continue;
+    }
+    const { val, left, right } = node;
+    if (val >= max) {
+      count++;
+    }
+    const m = Math.max(val, max);
+    stack.push([right, m], [left, m]);
+  }
+  return count;
+}
