2ce166684c292faa7e1d1d76c151c77309d0b62c
Yuria <nishimura.yuria@gmail.com>
2024-05-09 12:31:01 -0700

    Update 90. Subsets II solution (js)

--- LeetCode/90. Subsets II/solution.js
+++ LeetCode/90. Subsets II/solution.js
@@ -4,20 +4,20 @@
  */
 function subsetsWithDup(nums) {
   nums.sort((n1, n2) => n1 - n2);
-  const result = new Set();
+  const result = [];
   backtrack([], 0);
-  return Array.from(result).map((subset) =>
-    subset.length > 0 ? subset.split(",") : [],
-  );
+  return result;
 
   function backtrack(subset, index) {
-    result.add(subset.join(","));
-    if (index === nums.length) {
-      return;
+    result.push([...subset]);
+
+    for (let i = index; i < nums.length; i++) {
+      if (i !== index && nums[i] === nums[i - 1]) {
+        continue;
+      }
+      subset.push(nums[i]);
+      backtrack(subset, i + 1);
+      subset.pop();
     }
-    subset.push(nums[index]);
-    backtrack(subset, index + 1);
-    subset.pop();
-    backtrack(subset, index + 1);
   }
 }
