0956f9bdbbed7122fad86bad673b89e08775841b
Yuria <nishimura.yuria@gmail.com>
2024-06-05 18:46:37 -0700

    Update 208. Implement Trie (Prefix Tree) solution (js)

--- LeetCode/208. Implement Trie (Prefix Tree)/solution.js
+++ LeetCode/208. Implement Trie (Prefix Tree)/solution.js
@@ -1,7 +1,75 @@
+// Trie (Prefix Tree) solution
+// time: O(1), memory: O(n)
 /**
  * Initialize your data structure here.
  */
+class TrieNode {
+  children = new Map();
+  word = false;
+}
+
 class Trie {
+  root = new TrieNode();
+
+  /**
+   * @param {string} word
+   * @return {void}
+   */
+  insert(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+      node = node.children.get(char);
+    }
+    node.word = true;
+  }
+
+  /**
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    let node = this.root;
+    for (const char of word) {
+      if (!node.children.has(char)) {
+        return false;
+      }
+      node = node.children.get(char);
+    }
+    return node.word;
+  }
+
+  /**
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    let node = this.root;
+    for (const char of prefix) {
+      if (!node.children.has(char)) {
+        return false;
+      }
+      node = node.children.get(char);
+    }
+    return true;
+  }
+}
+
+// Another solution
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+
+/**
+ * Initialize your data structure here.
+ */
+class Trie2 {
   constructor() {
     this.wordSet = new Set();
     this.prefixSet = new Set();
