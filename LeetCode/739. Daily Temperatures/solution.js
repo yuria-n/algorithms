0ae475c21260792fc76389c0b5d534a35999b9b5
Yuria <nishimura.yuria@gmail.com>
2024-06-17 14:17:29 -0700

    Update 739. Daily Temperatures solution (js)

--- LeetCode/739. Daily Temperatures/solution.js
+++ LeetCode/739. Daily Temperatures/solution.js
@@ -6,17 +6,15 @@ function dailyTemperatures(temperatures) {
   const result = new Array(temperatures.length).fill(0);
   const stack = [];
   for (let i = temperatures.length - 1; i >= 0; i--) {
-    let j = stack.length;
-    while (--j >= 0) {
-      const k = stack[j];
-      if (temperatures[k] > temperatures[i]) {
-        result[i] = k - i;
+    while (stack.length > 0) {
+      const j = stack[stack.length - 1];
+      if (temperatures[j] > temperatures[i]) {
+        result[i] = j - i;
         break;
       }
       stack.pop();
     }
     stack.push(i);
   }
-
   return result;
 }
