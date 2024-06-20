fe64f1e3c1cf03528e2ff4580661a0819126c452
Yuria <nishimura.yuria@gmail.com>
2024-06-20 15:49:00 -0700

    Update 973. K Closest Points to Origin solution (js)

--- LeetCode/973. K Closest Points to Origin/solution.js
+++ LeetCode/973. K Closest Points to Origin/solution.js
@@ -1,3 +1,76 @@
+// Solution 1: heap
+// time: O(n log n)
+class Heap {
+  heap = [[]];
+
+  constructor(points) {
+    for (let i = 0; i < points.length; i++) {
+      this.push(points[i], i);
+    }
+  }
+
+  push([x, y], index) {
+    this.heap.push([x * x + y * y, index]);
+
+    let i1 = this.heap.length - 1;
+    let i0 = Math.floor(i1 / 2);
+    let val1 = this.heap[i1]?.[0];
+    let val0 = this.heap[i0]?.[0] ?? Infinity;
+    while (i0 > 0 && val1 < val0) {
+      [this.heap[i0], this.heap[i1]] = [this.heap[i1], this.heap[i0]];
+      i1 = i0;
+      i0 = Math.floor(i1 / 2);
+      val1 = this.heap[i1]?.[0];
+      val0 = this.heap[i0]?.[0] ?? Infinity;
+    }
+  }
+
+  pop() {
+    const [, index] = this.heap[1];
+
+    this.heap[1] = this.heap.pop();
+    for (let ip = Math.floor(this.heap.length / 2); ip >= 0; ip--) {
+      let i = ip;
+      let il = 2 * i;
+      let ir = il + 1;
+      let val = this.heap[i][0];
+      let valL = this.heap[il]?.[0];
+      let valR = this.heap[ir]?.[0] ?? Infinity;
+      while (il < this.heap.length && val > Math.min(valL, valR)) {
+        if (valL < valR) {
+          [this.heap[i], this.heap[il]] = [this.heap[il], this.heap[i]];
+          i = il;
+        } else {
+          [this.heap[i], this.heap[ir]] = [this.heap[ir], this.heap[i]];
+          i = ir;
+        }
+        il = 2 * i;
+        ir = il + 1;
+        val = this.heap[i][0];
+        valL = this.heap[il]?.[0];
+        valR = this.heap[ir]?.[0];
+      }
+    }
+
+    return index;
+  }
+}
+
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+function kClosest(points, k) {
+  const result = [];
+  const heap = new Heap(points);
+  for (let i = 0; i < k; i++) {
+    result.push(points[heap.pop()]);
+  }
+  return result;
+}
+
+// Solution 2: sort
 /**
  * @param {number[][]} points
  * @param {number} K
