a2a7076748821ce04ff296a36367aa8f30395f94
Yuria <yuria.motegi@gmail.com>
2024-07-22 10:49:31 -0700

    Update 207. Course Schedule solution (js)

--- LeetCode/207. Course Schedule/solution.js
+++ LeetCode/207. Course Schedule/solution.js
@@ -4,19 +4,19 @@
  * @return {boolean}
  */
 function canFinish(numCourses, prerequisites) {
-  const preRequisite = Array.from({ length: numCourses }, () => []);
+  const adjacencyList = Array.from({ length: numCourses }, () => []);
   for (const [c, p] of prerequisites) {
-    preRequisite[c].push(p);
+    adjacencyList[c].push(p);
   }
   const taken = new Map();
-  return Array.from(preRequisite.keys()).every(takeCourse);
+  return adjacencyList.every((_, c) => takeCourse(c));
 
   function takeCourse(c) {
     if (taken.has(c)) {
       return taken.get(c);
     }
     taken.set(c, false);
-    if (!preRequisite[c].every(takeCourse)) {
+    if (!adjacencyList[c].every(takeCourse)) {
       return false;
     }
     taken.set(c, true);
