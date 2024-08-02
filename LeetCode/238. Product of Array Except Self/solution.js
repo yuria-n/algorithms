0f8fcc5f6d15325c55f070dc071a3455302ae2eb
Yuria <yuria.motegi@gmail.com>
2024-08-02 11:03:11 -0700

    Update 238. Product of Array Except Self solution (js)

--- LeetCode/238. Product of Array Except Self/solution.js
+++ LeetCode/238. Product of Array Except Self/solution.js
@@ -11,28 +11,8 @@ function productExceptSelf(nums) {
     const j = l - i - 1;
     result[i] *= p1;
     result[j] *= p2;
-    p1 = p1 * nums[i];
-    p2 = p2 * nums[j];
+    p1 *= nums[i];
+    p2 *= nums[j];
   }
   return result;
-}
-
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-function productExceptSelf(nums) {
-  const l = nums.length;
-
-  const prods1 = new Array(l).fill(1);
-  for (let i = 1; i < nums.length; i++) {
-    prods1[i] = prods1[i - 1] * nums[i - 1];
-  }
-
-  const prods2 = new Array(l).fill(1);
-  for (let i = nums.length - 2; i >= 0; i--) {
-    prods2[i] = prods2[i + 1] * nums[i + 1];
-  }
-
-  return prods1.map((num, i) => num * prods2[i]);
 }
