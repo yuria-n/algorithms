c03cc7106b9d20d8b428f208590ae100f01d6ce7
Yuria Motegi <yuria.motegi@gmail.com>
2025-10-13 21:10:11 -0700

    Update 57. Insert Interval solution (js)

--- LeetCode/57. Insert Interval/solution.js
+++ LeetCode/57. Insert Interval/solution.js
@@ -22,3 +22,33 @@ function insert(intervals, newInterval) {
   result.push(newInterval);
   return result;
 }
+
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+function insert2(intervals, newInterval) {
+    const [newStart, newEnd] = newInterval;
+    let start = -1;
+    let end = -1;
+
+    for (let i = 0; i < intervals.length; i++) {
+        const [s, e] = intervals[i];
+        
+        if (start === -1 && e >= newStart) {
+            start = i;
+        }
+        
+        if (s <= newEnd) {
+            end = i;
+        }
+    }
+
+    return [
+        ...intervals.slice(0, start > -1 ? start : intervals.length),
+        [Math.min(intervals[start]?.[0] ?? Infinity, newStart), Math.max(intervals[end]?.[1] ?? -Infinity, newEnd)],
+        ...intervals.slice(end + 1)
+    ];
+};
