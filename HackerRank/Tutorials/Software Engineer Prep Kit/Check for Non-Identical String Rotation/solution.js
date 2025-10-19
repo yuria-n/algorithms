65be9724d7d43031a07046954a0fca48511018cd
Yuria Motegi <yuria.motegi@gmail.com>
2025-10-18 18:40:34 -0700

    Update Check for Non-Identical String Rotation solution (js)

--- HackerRank/Tutorials/Software Engineer Prep Kit/Check for Non-Identical String Rotation/solution.js
+++ HackerRank/Tutorials/Software Engineer Prep Kit/Check for Non-Identical String Rotation/solution.js
@@ -8,20 +8,5 @@
  */
 
 function isNonTrivialRotation(s1, s2) {
-  if (s1 === s2) {
-    return 0;
-  }
-
-  const l = s1.length;
-
-  root: for (let i = 0; i < l; i++) {
-    for (let j = 0; j < l; j++) {
-      if (s1[j] !== s2[(j + i) % l]) {
-        continue root;
-      }
-    }
-    return 1;
-  }
-
-  return 0;
+  return s1 !== s2 && (s1 + s1).includes(s2) ? 1 : 0;
 }
