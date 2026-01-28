1a0f77c382b657ee7390b0383f178beee40306bc
Yuria Motegi <yuria.motegi@gmail.com>
2026-01-27 20:13:35 -0800

    Update debounce (ts)

--- Others/Util Functions/debounce.ts
+++ Others/Util Functions/debounce.ts
@@ -1,32 +1,43 @@
 // _.debounce
 
-function debounce(func, wait) {
-  let prev = null;
-  let cb = null;
-  f.cancel = cancel;
-  f.flush = flush;
-  return f;
+type DebouncedFunction<T extends (...args: any[]) => any> = {
+  (...args: Parameters<T>): void;
+  cancel(): void;
+  flush(): void;
+};
 
-  function f(...args) {
+function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+): DebouncedFunction<T> {
+  let prev: NodeJS.Timeout | null = null;
+  let cb: (() => void) | null = null;
+
+  function debouncedFunc(...args: Parameters<T>): void {
     cancel();
     cb = () => func(...args);
     prev = setTimeout(cb, wait);
   }
 
-  function cancel() {
-    if (prev === null) {
-      return;
+  function cancel(): void {
+    if (prev !== null) {
+      clearTimeout(prev);
+      prev = null;
     }
-    clearTimeout(prev);
-    prev = null;
   }
 
-  function flush() {
-    if (cb === null) {
-      return;
+  function flush(): void {
+    if (cb !== null) {
+      cancel();
+      cb();
+      cb = null;
     }
-    cancel();
-    cb();
-    cb = null;
   }
+
+  debouncedFunc.cancel = cancel;
+  debouncedFunc.flush = flush;
+
+  return debouncedFunc as DebouncedFunction<T>;
 }
+
+export default debounce;
